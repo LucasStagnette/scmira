c3774b853175ba10371618000742894ea4f1cbb5
LucasStagnette <103002404+LucasStagnette@users.noreply.github.com>
2023-04-11 12:03:21 +0200

    Add files via upload

---
+++ scmira/clients/add_client.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <link rel="stylesheet" type="text/css" href="./style.css" />
+    <meta charset="utf-8" />
+	<title>Scimera</title>
+</head>
+<body>
+</body>
+</html>
---
+++ scmira/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <link rel="stylesheet" type="text/css" href="./style.css" />
+    <meta charset="utf-8" />
+	<title>Scimera</title>
+ </head>
+<body>
+	<div>
+		<!-- Bouton qui vérifie la vanne -->
+		<input type="submit" name="repere" value="Repère Vanne">
+		<!-- Bouton pour afficher les clients -->
+		<button><a href="clients/view_clients.php">Clients</a></button>
+	</div>
+</body>
+</html>
