c45c3141c28444b93807b864f542289fa32d2760
LucasStagnette <103002404+LucasStagnette@users.noreply.github.com>
2023-04-14 12:06:14 +0200

    Add files via upload

---
+++ scmira/clients/view_clients.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <link rel="stylesheet" type="text/css" href="../style.css" />
+    <meta charset="utf-8" />
+	<title>Clients</title>
+</head>
+<body>
+    
+</body>
+</html>
